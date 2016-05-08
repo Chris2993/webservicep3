3f6e4eab8b1673674ac2c51681d67a780bd5e472
Chris2993 <christian_2993@hotmail.com>
2016-05-08 18:32:11 -0500

    Revert "WebService"
    This reverts commit 157a87e3eea04599b571d7f672b39f1f258fb091.

--- datos/configuracion.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define("SERVIDOR_BD", "ec2-54-243-212-72.compute-1.amazonaws.com");
-define("USUARIO_BD", "nosawaqcsltjmw");
-define("CLAVE_BD", "k8UL-8x95j97CGuhf8jXOQM19R");
-define("PUERTO_BD", "5432");
-define("NOMBRE_BD", "d97fn2jkuiobr9");
